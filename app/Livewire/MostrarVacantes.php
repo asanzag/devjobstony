0758ad0bd2a7610cc6fe9129594305b351c59350
Antonio <asanzag@gmail.com>
2024-02-16 18:59:44 +0100

    first commit

---
+++ app/Livewire/MostrarVacantes.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Vacante;
+use Livewire\Component;
+
+class MostrarVacantes extends Component
+{
+    // protected $listeners = ['prueba'];
+
+    // public function prueba($id)
+    // {
+    //     dd($id);
+    // }
+
+    public function render()
+    {
+        $vacantes = Vacante::where('user_id', auth()->user()->id)->paginate(10);
+
+        return view('livewire.mostrar-vacantes', [
+            'vacantes' => $vacantes
+        ]);
+    }
+}
